460db54b9aba442ccc73da02509181a399ce75b7
Johnx <hoanganh692004@gmail.com>
2023-01-15 22:06:36 -0500

    Add file

---
+++ App_Login/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'App_Login'
+urlpatterns = [
+    path('signup/', views.sign_up, name='signup'),
+    path('signin/', views.login_page, name='signin'),
+    path('logout/', views.logout_user, name='logout'),
+    path('profile/', views.profile, name='profile')
+]
