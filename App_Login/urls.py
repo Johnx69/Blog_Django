414adb6d1b84403ba8b46e6b9c72cf3601605a5e
Johnx <hoanganh692004@gmail.com>
2023-01-16 21:02:49 -0500

    update

--- App_Login/urls.py
+++ App_Login/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('signup/', views.sign_up, name='signup'),
     path('signin/', views.login_page, name='signin'),
     path('logout/', views.logout_user, name='logout'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.profile, name='profile'),
+    path('change-profile/', views.user_change, name='user_change'),
+    path('password/', views.pass_change, name='pass_change'),
+    path('change-profile-image/', views.add_pro_pic, name='add_pro_pic'),
 ]
